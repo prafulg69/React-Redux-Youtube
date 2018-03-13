aa42df832a79f9c33de99ba265db52e9b2d65d00
PRAFUL GHADGE <PRAFUL GHADGE>
2018-03-13 22:38:00 +0530

    add the search code

--- src/index.js
+++ src/index.js
@@ -16,18 +16,24 @@ class App extends Component {
      videos: [],
      selectedVideo: null
      };
+     this.videoSearch('chinmaya');
+} 
 
-  YTSearch({key: API_KEY, term: 'chinmayamission'}, (videos) => {
-   this.setState({ 
-     videos: videos,
-     selectedVideo: videos[0] 
-      });
-    });
-  } 
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0] 
+         });
+       });
+  }
+
+
   render() {
   return (
   <div>
-    <SearchBar />
+    <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
     <VideoDetail video={this.state.selectedVideo} />
     <VideoList 
     onVideoSelect={selectedVideo => this.setState({selectedVideo})}
